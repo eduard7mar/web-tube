05c742eff059a202785cae721b63a4fcd7acf64c
eduard7mar <edwardmargaryan@gmail.com>
2024-03-12 20:37:50 +0200

    added header buttons

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,18 +1,51 @@
-import { Menu } from "lucide-react";
+import { Menu, Upload, Bell, User, Mic, Search } from "lucide-react";
 import logo from "../assets/Logo.png";
 import { Button } from "../components/Button";
 
 export function PageHeader() {
-    return <div className="flex gap-10 lg:gap-20 justify-between">
-        <div className="flex gap-4 items-center flex-shrink-0">
-            <Button>
-                <Menu />
-            </Button>
-            <a href="/">
-                <img src={logo} className="h-6" alt="logo" />
-            </a>
+  return (
+    <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
+      <div className="flex gap-4 items-center flex-shrink-0">
+        <Button>
+          <Menu />
+        </Button>
+        <a href="/">
+          <img src={logo} className="h-6" alt="logo" />
+        </a>
+      </div>
+      <form className="md:flex hidden gap-4 flex-grow justify-center">
+        <div className="flex flex-grow max-w-[600px]">
+          <input
+            type="search"
+            placeholder="Search"
+            className="rounded-l-full border border-secondary-border shadow-inner shadow-secondary
+          py-1 px-4 text-lg w-full focus:border-blue-500 outline-none"
+          />
+          <Button className="py-2 px-4 rounded-r-full border-secondary-border border border-l-0 flex-shrink-0">
+            <Search />
+          </Button>
         </div>
-        <div></div>
-        <div></div>
-    </div>;
+        <Button type="button" size="icon" className="flex-shrink-0">
+          <Mic />
+        </Button>
+      </form>
+      <div className="flex flex-shrink-0 md:gap-2">
+        <Button size="icon" variant="ghost" className="md:hidden">
+          <Search />
+        </Button>
+        <Button size="icon" variant="ghost" className="md:hidden">
+          <Mic />
+        </Button>
+        <Button size="icon" variant="ghost">
+          <Upload />
+        </Button>
+        <Button size="icon" variant="ghost">
+          <Bell />
+        </Button>
+        <Button size="icon" variant="ghost">
+          <User />
+        </Button>
+      </div>
+    </div>
+  );
 }
